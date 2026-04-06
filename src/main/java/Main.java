949f93f77d5e350ed71b890cac637a1ccf5f9164
Raul José Chaves <rjchaves93@gmail.com>
2026-04-06 20:57:01 +0100

    Print page size

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -16,8 +15,8 @@ public class Main {
     switch (command) {
       case ".dbinfo" -> {
         try {
-          FileInputStream databaseFile = new FileInputStream(new File(databaseFilePath));
-          
+          FileInputStream databaseFile = new FileInputStream(databaseFilePath);
+
           databaseFile.skip(16); // Skip the first 16 bytes of the header
           byte[] pageSizeBytes = new byte[2]; // The following 2 bytes are the page size
           databaseFile.read(pageSizeBytes);
@@ -28,7 +27,7 @@ public class Main {
           System.err.println("Logs from your program will appear here!");
 
           // TODO: Uncomment the code below to pass the first stage
-          // System.out.println("database page size: " + pageSize);
+           System.out.println("database page size: " + pageSize);
         } catch (IOException e) {
           System.out.println("Error reading file: " + e.getMessage());
         }

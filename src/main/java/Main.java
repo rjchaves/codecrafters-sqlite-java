e82755fce6c143431c42acc1eb348453b87a7a60
Raul José Chaves <rjchaves93@gmail.com>
2026-04-06 22:00:11 +0100

    Cleaning up to java 25

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,34 @@
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-
-public class Main {
-  public static void main(String[] args){
+void main(String[] args) {
     if (args.length < 2) {
-      System.out.println("Missing <database path> and <command>");
-      return;
+        IO.println("Missing <database path> and <command>");
+        return;
     }
 
     String databaseFilePath = args[0];
     String command = args[1];
 
     switch (command) {
-      case ".dbinfo" -> {
-        try {
-          FileInputStream databaseFile = new FileInputStream(databaseFilePath);
+        case ".dbinfo" -> {
+            try {
+                FileInputStream databaseFile = new FileInputStream(databaseFilePath);
 
-          databaseFile.skipNBytes(16); // Skip the first 16 bytes of the header
-          byte[] pageSizeBytes = new byte[2]; // The following 2 bytes are the page size
-          databaseFile.read(pageSizeBytes);
-          short pageSizeSigned = ByteBuffer.wrap(pageSizeBytes).getShort();
-          int pageSize = Short.toUnsignedInt(pageSizeSigned);
-          byte[] pageHeader = new byte[2];
-          databaseFile.skipNBytes(85);
-          databaseFile.read(pageHeader);
-          short numberOfTablesSigned = ByteBuffer.wrap(pageHeader).getShort();
-          int numberOfTables = Short.toUnsignedInt(numberOfTablesSigned);
+                databaseFile.skipNBytes(16); // Skip the first 16 bytes of the header
+                byte[] pageSizeBytes = new byte[2]; // The following 2 bytes are the page size
+                databaseFile.read(pageSizeBytes);
+                short pageSizeSigned = ByteBuffer.wrap(pageSizeBytes).getShort();
+                int pageSize = Short.toUnsignedInt(pageSizeSigned);
+                byte[] pageHeader = new byte[2];
+                databaseFile.skipNBytes(85);
+                databaseFile.read(pageHeader);
+                short numberOfTablesSigned = ByteBuffer.wrap(pageHeader).getShort();
+                int numberOfTables = Short.toUnsignedInt(numberOfTablesSigned);
 
-           System.out.println("database page size: " + pageSize);
-           System.out.println("number of tables: " + numberOfTables);
-        } catch (IOException e) {
-          System.out.println("Error reading file: " + e.getMessage());
+                IO.println("database page size: " + pageSize);
+                IO.println("number of tables: " + numberOfTables);
+            } catch (IOException e) {
+                IO.println("Error reading file: " + e.getMessage());
+            }
         }
-      }
-      default -> System.out.println("Missing or invalid command passed: " + command);
+        default -> IO.println("Missing or invalid command passed: " + command);
     }
-  }
 }
